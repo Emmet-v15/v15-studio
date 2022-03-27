83209f16cb6cc990c3cb216ebabd1d374db29ff5
Emmetv15 <emmettrey726@gmail.com>
2022-03-28 00:35:34 +0100

    fixing api

--- api/router.js
+++ api/router.js
@@ -4,7 +4,7 @@ const router = Router();
 router.get("*", function (req, res, next) {
     console.log(req.ip);
     if (!req.url.startsWith("/api/v1")) res.redirect("/api/v1" + req.url);
-    res.send(200);
+    // res.send(200);
     next();
 });
 
