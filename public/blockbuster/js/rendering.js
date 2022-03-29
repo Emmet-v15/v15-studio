5c22de6eabcbf1be6f47d8777ffee9e20e224518
Emmetv15 <emmettrey726@gmail.com>
2022-03-29 11:58:55 +0100

    blockbuster update

---
+++ public/blockbuster/js/rendering.js
@@ -0,0 +1,57 @@
+export class Rendering {
+    constructor() {
+        /** @type {CanvasRenderingContext2D} */
+        this.canvas;
+        this.ctx;
+        this.mouse = { down: [false, false, false], x: 0, y: 0 };
+
+        window.requestAnimFrame =
+            window.requestAnimationFrame ||
+            window.webkitRequestAnimationFrame ||
+            window.mozRequestAnimationFrame ||
+            window.oRequestAnimationFrame ||
+            window.msRequestAnimationFrame ||
+            function (callback) {
+                window.setTimeout(callback, 1000 / 60);
+            };
+
+        window.onload = () => {
+            this.canvas = document.getElementById("canvas");
+            this.canvas.width = innerWidth;
+            this.canvas.height = innerHeight;
+
+            this.ctx = canvas.getContext("2d", { alpha: false });
+
+            document.oncontextmenu = (e) => {
+                e.preventDefault();
+            };
+            document.addEventListener("mousemove", (event) => {
+                (mouse.x = event.clientX), (mouse.y = event.clientY);
+            });
+            document.addEventListener("mousedown", (event) => {
+                hexes.updateClicks();
+                mouse.down[event.button] = true;
+                event.preventDefault();
+            });
+            document.addEventListener("mouseup", (event) => {
+                mouse.down[event.button] = false;
+                event.preventDefault();
+            });
+
+            // document.onkeydown = (event) => {
+            //     switch (event.key) {
+            //         case "c":
+            //             console.log("pressed c");
+            //             break;
+            //     }
+            // };
+
+            this.start();
+            requestAnimFrame(this.update);
+        };
+    }
+    start() {}
+    update() {
+        requestAnimFrame(this.update);
+    }
+}
