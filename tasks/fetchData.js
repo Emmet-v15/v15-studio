0e6313ee3c9b868a9d44e9ac4b393b73e64148fb
Emmetv15 <emmettrey726@gmail.com>
2022-05-03 23:31:21 +0100

    stuff

--- tasks/fetchData.js
+++ tasks/fetchData.js
@@ -13,49 +13,51 @@ async function fetchTimetable() {
         args: ["--no-sandbox", "--disable-setuid-sandbox"],
     });
     saveThumbnail(browser);
-    // const page = await browser.newPage();
-    // await page.authenticate({ username: username, password: password });
+    const page = await browser.newPage();
+    await page.authenticate({ username: username, password: password });
 
-    // await page.goto(timetableURL, { waitUntil: "networkidle2" }).catch((e) => void 0);
+    await page.goto(timetableURL, { waitUntil: "networkidle2" }).catch((e) => void 0);
 
-    // const cells = await page.evaluate(() => {
-    //     const rows = document.querySelectorAll("#Content_Content_Content_MainContent_timetable1_tbltimetable tr");
-    //     return Array.from(rows, (row) => {
-    //         const columns = row.querySelectorAll("td");
-    //         return Array.from(columns, (column) => column.innerText);
-    //     });
-    // });
-    // if (cells.length == 0) {
-    //     console.log("Failed to fetch, Retrying...");
-    //     fetchTimetable();
-    //     return;
-    // }
-    // page.close();
-    // data = {
-    //     timestamp: Date.now() + 60 * 60 * 1000,
-    //     cells: cells.slice(0),
-    // };
-    // fs.writeFileSync(
-    //     path.join(__dirname, "../public/timetable/timetableData.json"),
-    //     JSON.stringify({ data: data }, null, 4)
-    // );
+    const cells = await page.evaluate(() => {
+        const rows = document.querySelectorAll("#Content_Content_Content_MainContent_timetable1_tbltimetable tr");
+        return Array.from(rows, (row) => {
+            const columns = row.querySelectorAll("td");
+            return Array.from(columns, (column) => column.innerText);
+        });
+    });
+    if (cells.length == 0) {
+        console.log("Failed to fetch, Retrying...");
+        fetchTimetable();
+        return;
+    }
+    page.close();
+    data = {
+        timestamp: Date.now() + 60 * 60 * 1000,
+        cells: cells.slice(0),
+    };
+    fs.writeFileSync(
+        path.join(__dirname, "../public/timetable/timetableData.json"),
+        JSON.stringify({ data: data }, null, 4)
+    );
 
-    // setTimeout(() => {
-    //     saveThumbnail(browser);
-    // }, 1 * 1000);
+    setTimeout(() => {
+        saveThumbnail(browser);
+    }, 1 * 1000);
 
-    // setTimeout(fetchTimetable, 60 * 60 * 1000);
+    setTimeout(fetchTimetable, 60 * 60 * 1000);
 }
 
 async function saveThumbnail(browser) {
-    http.get("http://example.com/category", async (res) => {
+    http.get("https://v15.studio/timetable", async (res) => {
         const page = await browser.newPage();
         await page.goto("https://v15.studio/timetable", { waitUntil: "networkidle2" }).catch((e) => void 0);
-        await page.setViewport({ width: 1200, height: 600, deviceScaleFactor: 2 });
-        await page.screenshot({
-            path: path.join(__dirname, "../public/timetable/thumbnail.png"),
-            fullPage: true,
-        });
+        await page.setViewport({ width: 1200, height: 600, deviceScaleFactor: 1 });
+        await page
+            .screenshot({
+                path: path.join(__dirname, "../public/timetable/thumbnail.png"),
+                fullPage: true,
+            })
+            .catch((e) => void 0);
         await browser.close();
     }).on("error", function (e) {
         console.log("Failed to create new Thumbnail.");
