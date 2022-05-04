7a1c1a0df5285cdf41780dd85f3a96ecfc903a9f
Emmetv15 <emmettrey726@gmail.com>
2022-05-04 18:52:37 +0100

    image cropper

--- tasks/fetchData.js
+++ tasks/fetchData.js
@@ -4,6 +4,7 @@ const fs = require("fs");
 const path = require("path");
 const { username, password, timetableURL } = require("../config.json");
 const http = require("http");
+const sharp = require("sharp");
 var data = {};
 
 async function fetchTimetable() {
@@ -12,40 +13,40 @@ async function fetchTimetable() {
         userDataDir: "./cache",
         args: ["--no-sandbox", "--disable-setuid-sandbox"],
     });
-    // saveThumbnail(browser);
-    const page = await browser.newPage();
-    await page.authenticate({ username: username, password: password });
+    saveThumbnail(browser);
+    // const page = await browser.newPage();
+    // await page.authenticate({ username: username, password: password });
 
-    await page.goto(timetableURL, { waitUntil: "networkidle2" }).catch((e) => void 0);
+    // await page.goto(timetableURL, { waitUntil: "networkidle2" }).catch((e) => void 0);
 
-    const cells = await page.evaluate(() => {
-        const rows = document.querySelectorAll("#Content_Content_Content_MainContent_timetable1_tbltimetable tr");
-        return Array.from(rows, (row) => {
-            const columns = row.querySelectorAll("td");
-            return Array.from(columns, (column) => column.innerText);
-        });
-    });
-    if (cells.length == 0) {
-        console.log("Failed to fetch, Retrying...");
-        await browser.close();
-        fetchTimetable();
-        return;
-    }
-    page.close();
-    data = {
-        timestamp: Date.now() + 60 * 60 * 1000,
-        cells: cells.slice(0),
-    };
-    fs.writeFileSync(
-        path.join(__dirname, "../public/timetable/timetableData.json"),
-        JSON.stringify({ data: data }, null, 4)
-    );
+    // const cells = await page.evaluate(() => {
+    //     const rows = document.querySelectorAll("#Content_Content_Content_MainContent_timetable1_tbltimetable tr");
+    //     return Array.from(rows, (row) => {
+    //         const columns = row.querySelectorAll("td");
+    //         return Array.from(columns, (column) => column.innerText);
+    //     });
+    // });
+    // if (cells.length == 0) {
+    //     console.log("Failed to fetch, Retrying...");
+    //     await browser.close();
+    //     fetchTimetable();
+    //     return;
+    // }
+    // page.close();
+    // data = {
+    //     timestamp: Date.now() + 60 * 60 * 1000,
+    //     cells: cells.slice(0),
+    // };
+    // fs.writeFileSync(
+    //     path.join(__dirname, "../public/timetable/timetableData.json"),
+    //     JSON.stringify({ data: data }, null, 4)
+    // );
 
-    setTimeout(() => {
-        saveThumbnail(browser);
-    }, 1 * 1000);
+    // setTimeout(() => {
+    //     saveThumbnail(browser);
+    // }, 1 * 1000);
 
-    setTimeout(fetchTimetable, 60 * 60 * 1000);
+    // setTimeout(fetchTimetable, 60 * 60 * 1000);
 }
 
 async function saveThumbnail(browser) {
@@ -55,12 +56,21 @@ async function saveThumbnail(browser) {
         await page.setViewport({ width: 1200, height: 600, deviceScaleFactor: 1 });
         await page
             .screenshot({
-                path: path.join(__dirname, "../public/timetable/thumbnail.png"),
+                path: path.join(__dirname, "/thumbnail-temp.png"),
                 fullPage: true,
             })
             .catch((e) => void 0);
         await browser.close();
-    }).on("error", function (e) {
+        sharp(path.join(__dirname, "/thumbnail-temp.png"))
+            .extract({ width: 960, height: 480, left: 120, top: 120 })
+            .toFile(path.join(__dirname + "/../public/timetable/thumbnail.png"))
+            .then(() => {
+                console.log("Image cropped and saved");
+            })
+            .catch((e) => {
+                console.log(e);
+            });
+    }).on("error", (e) => {
         browser.close();
         console.log("Failed to create new Thumbnail.");
     });
