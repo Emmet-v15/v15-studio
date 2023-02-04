7bcf1a30cd6e73c4633d98f40e581b473b731e7a
Emmet_v15 <emmettrey726@gmail.com>
2023-02-04 19:37:42 +0000

    update

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ const puppeteer = require("puppeteer");
 
 (async () => {
     const browser = await puppeteer.launch({
-        headless: true,
+        devtools: false,
         args: ["--no-sandbox", "--disable-setuid-sandbox"],
     });
 
