af03fc7ae3fa1fa4226fad8e6f62835a32317994
Emmet <emmettrey726@gmail.com>
2023-09-22 00:20:16 +0100

    api updates

--- api/router.js
+++ api/router.js
@@ -10,4 +10,13 @@ router.get("/api/v1", function (req, res) {
     res.send("Emmet's API");
 });
 
+router.post("/api/v1/pollbot", function (req, res) {
+    const body = req.body;
+    if (!body) return;
+    if (body.method == "upvote") {
+        console.log("Method: Up Vote");
+        console.log("Post^: " + body.post);
+    }
+});
+
 module.exports = router;
